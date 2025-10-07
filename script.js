8cd1018a10d7b912cf077ec7e6f6130d9857768f
Jo Greenlee <sweettroubles29@gmail.com>
2025-10-07 17:35:28 -0500

    Excercise 3

--- script.js
+++ script.js
@@ -2,25 +2,42 @@ const book = {
     title: "Pride And Prejudice",
     author: "Jane Austen",
     pages: 429,
-    read: function() {
+    read: function () {
         console.log('Currently reading "${this.title}" by ${this.author}.');
     }
 };
 
 book.read();
 
-function Animal(species,name,sound) {
+function Animal(species, name, sound) {
     this.species = species;
     this.name = name;
     this.sound = sound;
-    this.makesound = function() {
-console.log('${this.name} says ${this.sound}');
+    this.makesound = function () {
+        console.log('${this.name} says ${this.sound}');
     };
 }
 
 const animal1 = new Animal("Cow", "Betsy", "Moo!");
 const animal2 = new Animal("Duck", "Howard", "Quack!");
 
-animal1.makesound(); 
+animal1.makesound();
 animal2.makesound();
 
+function Student(name, grade, subject) {
+    this.name = name;
+    this.grade = grade;
+    this.subject = subject;
+
+    this.introduce = function () {
+        console.log('Hi, I am ${this.name}, studying ${this.subject} in grade $this.grade}.');
+};
+this.finishedSchoolYear = function() {
+if (this.grade < 12) {
+    this.grade++;
+    console.log('${this.name} is now in grade ${this.grade}.');
+    } else {
+        console.log('Congratulations, ${this.name} has graduated!');
+    }
+};
+}
