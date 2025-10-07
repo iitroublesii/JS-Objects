77ac8eecdd2769580f9f6a89696839db55449fc6
Jo Greenlee <sweettroubles29@gmail.com>
2025-10-07 17:02:09 -0500

    Object Excercise 1

---
+++ script.js
@@ -0,0 +1,8 @@
+const book = {
+    title: "Pride And Prejudice",
+    author: "Jane Austen",
+    pages: 429,
+    read: function() {
+        console.log('Currently reading "${this.title}" by ${this.author}.');
+    }
+}
