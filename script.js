a0621894ecb961372e6d0c1198dcec9fe6af50dd
Jo Greenlee <sweettroubles29@gmail.com>
2025-10-07 17:45:46 -0500

    Updated Excercise 3

--- script.js
+++ script.js
@@ -31,13 +31,28 @@ function Student(name, grade, subject) {
 
     this.introduce = function () {
         console.log('Hi, I am ${this.name}, studying ${this.subject} in grade $this.grade}.');
-};
-this.finishedSchoolYear = function() {
-if (this.grade < 12) {
-    this.grade++;
-    console.log('${this.name} is now in grade ${this.grade}.');
-    } else {
-        console.log('Congratulations, ${this.name} has graduated!');
-    }
-};
+    };
+
+
+    this.finishedSchoolYear = function () {
+        if (this.grade < 12) {
+            this.grade++;
+            console.log('${this.name} is now in grade ${this.grade}.');
+        } else {
+            console.log('Congratulations, ${this.name} has graduated!');
+        }
+    };
 }
+
+const student1 = new Student("Jayden", 11, "English");
+const student2 = new Student("Aiden", 4, "History");
+const student3 = new Student("Kayden", 12, "Science");
+
+student1.introduce();
+student2.introduce();
+student3.introduce();
+
+student1.finishesSchoolYear();
+student2.finishesSchoolYear();
+student3.finishesSchollYear();
+
