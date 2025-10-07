ef58ff449bb3f362c3e4226d3acc0801c90d426e
Jo Greenlee <sweettroubles29@gmail.com>
2025-10-07 17:17:27 -0500

    Updated Constructor Function

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const book = {
 };
 
 book.read();
+
+function Animal(species,name,sound) {
+    this.species = species;
+    this.name = name;
+    this.sound = sound;
+    this.makesound = function() {
+console.log('${this.name} says ${this.sound}');
+    };
+}
+
+const animal1 = new Animal("Cow", "Betsy", "Moo!");
+const animal2 = new Animal("Duck", "Howard", "Quack!");
+
+animal1.makesound(); 
+animal2.makesound();
+
